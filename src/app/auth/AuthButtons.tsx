55b0b27cf634133f7f838214c4f3114addf9b97d
Aisha Najeeb <90481018+ayshanjb25@users.noreply.github.com>
2024-07-18 20:22:58 +0530

    Implement authentication

--- src/app/auth/AuthButtons.tsx
+++ src/app/auth/AuthButtons.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useState } from 'react';
+
 
 const AuthButtons = () => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className='flex gap-3 flex-1 md:flex-row flex-col relative z-50'>
-      <Button className='w-full' variant={"outline"}>Sign up</Button>
-      <Button className='w-full'>Login</Button>
-      
+
+      <RegisterLink className="flex-1" onClick={()=> setIsLoading(true)}>
+        <Button className='w-full' variant={"outline"} disabled={isLoading}>Sign up</Button>
+        </RegisterLink>
+      <LoginLink className="flex-1" onClick={()=> setIsLoading(true)}> 
+        <Button className='w-full' disabled={isLoading}>Login</Button>
+        </LoginLink>
+
     </div>
   )
 }
--- src/components/chat/ChatBottomBar.tsx
+++ src/components/chat/ChatBottomBar.tsx
@@ -5,12 +5,25 @@ import { useRef, useState } from 'react';
 import EmojiPicker from './EmojiPicker';
 import { Button } from '../ui/button';
 import {SendHorizontal, ThumbsUp, Loader} from 'lucide-react';
-
+import {useSound} from 'use-sound';
+import { usePreferences } from '@/store/usePreferences';
 
 const ChatBottomBar = () => {
 const [message, setMessage] = useState('');
 const textAreaRef = useRef<HTMLTextAreaElement>(null);
 const isPending = false;
+const [playKeySound1] = useSound("/sounds/keystroke1.mp3",{volume:0.3});
+const [playKeySound2] = useSound("/sounds/keystroke2.mp3",{volume:0.3});
+const [playKeySound3] = useSound("/sounds/keystroke3.mp3",{volume:0.3});
+const [playKeySound4] = useSound("/sounds/keystroke4.mp3",{volume:0.3});
+const { soundEnabled } = usePreferences();
+const playSoundFunction = [playKeySound1, playKeySound2, playKeySound3, playKeySound4];
+
+const playRandomKeystrokeSound = () => {
+    const randomIndex = Math.floor(Math.random() * playSoundFunction.length);
+    soundEnabled && playSoundFunction[randomIndex]();
+}
+
     return (
         <div className='p-2 flex justify-between w-full items-center gap-2'>
            {!message.trim() && <ImageIcon size={20} className="cursor-pointer text-muted-foreground" />} 
@@ -38,8 +51,12 @@ const isPending = false;
                         rows={1}
                         className="w-full border rounded-full flex items-center h-9 resize-none overflow-hidden bg-background min-h-0" 
                         value={message}
-                        onChange={(e)=> setMessage(e.target.value)}
-                        ref={textAreaRef}/>
+                        onChange={(e)=> {
+                            setMessage(e.target.value);
+                            playRandomKeystrokeSound();
+                        }}
+                        ref={textAreaRef}
+                       />
                         <div className='absolute right-2 bottom-0.5'>
                             <EmojiPicker onChange={(emoji)=>{
                                 setMessage(message + emoji);
